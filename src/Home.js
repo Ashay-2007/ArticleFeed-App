3f2cfb959c1c18ea36e4f5d40fa1eaefe46ff695
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-26 01:35:50 +0530

    removed unwanted images

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import "./Home.css";
 import Card from './Card';
-import img2 from "./images/img2.jpg";
-import img3 from "./images/img3.jpg";
-import img4 from "./images/img4.jpg";
-import img5 from "./images/img5.jpg";
-import img7 from "./images/img7.jpg";
-import img8 from "./images/img8.jpg";
-import img9 from "./images/img9.jpg";
 import { Link } from 'react-router-dom';
 
 
@@ -16,28 +9,28 @@ function Home() {
         <div>
             <img 
 				className="home__image"
-                src={img2}
+                src="https://images.unsplash.com/photo-1604696734183-cc0ab548b909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
                 alt =""
             />
             
             <div className="home__cards bd-grid">
                 <Link to="/sports">
-                    <Card img={img3} title="Sports"/>
+                    <Card title="Sports" img="https://images.unsplash.com/photo-1517649763962-0c623066013b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
                 </Link>
                 <Link to="/politics">
-                    <Card img={img4} title="Politics"/>
+                    <Card title="Politics" img="https://images.unsplash.com/photo-1541872703-74c5e44368f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=995&q=80"/>
                 </Link>
                 <Link to="/space">
-                    <Card img={img5} title="Space"/>                
+                    <Card title="Space" img="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80" />                
                 </Link>
                 <Link to="technology">
-                    <Card img={img7} title="Technology"/>
+                    <Card title="Technology" img="https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
                 </Link>
                 <Link to="travel">
-                    <Card img={img8} title="Travel"/>
+                    <Card title="Travel" img="https://images.unsplash.com/photo-1508672019048-805c876b67e2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1093&q=80" />
                 </Link>
                 <Link to="/fashion">
-                    <Card img={img9} title="Fashion"/>
+                    <Card title="Fashion" img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
                 </Link>
                 
                 
